6e6ba7935544ef520f77dde1f878febcf927259d
Iryna Shymbor <i.shymbor@gmail.com>
2020-10-26 16:16:47 +1100

    added React Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import WellnessWheel from "./WellnessWheel";
+import { Route, Switch } from "react-router-dom";
 // import "../style.css";
 
 export default function App() {
   return (
     <div>
-      {/*<CanvasShape width={300} height={300} /> */}
-      <WellnessWheel radiiArr={[4, 2, 3, 5, 1, 4, 3, 1]} />
+      <Switch>
+        {/* this works but not the best practice: */}
+        {/* <Route exact path='/wheel' component={() => <WellnessWheel radiiArr={[4, 2, 3, 5, 1, 4, 3, 1]} />} /> */}{" "}
+        <Route
+          exact
+          path="/wheel"
+          render={(props) => (
+            <WellnessWheel {...props} radiiArr={[4, 2, 3, 5, 1, 4, 3, 1]} />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
