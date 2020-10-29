5fd703d89ca639ebe42e1bfd437f2f8c484ec545
Iryna Shymbor <i.shymbor@gmail.com>
2020-10-29 19:24:00 +1100

    added labels to Wheel component

--- src/components/WellnessWheel.jsx
+++ src/components/WellnessWheel.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 class WellnessWheel extends React.Component {
-
   state = {
     bigRad: 100,
     radiiArr: this.props.radiiArr.map((rad) => rad * 20),
-    numberOfSlices: 8,
-    colorArr: ['#f46464', "#fed6a8", "#53b89a", "#008080", "#38a9c7", "#445588", "#d43e8c", "#bd121b"],
+    numberOfSlices: this.props.radiiArr.length,
+    colorArr: [
+      "#f46464",
+      "#fed6a8",
+      "#53b89a",
+      "#008080",
+      "#38a9c7",
+      "#445588",
+      "#d43e8c",
+      "#bd121b",
+    ],
   };
 
   drawCircle(bigRad) {
@@ -16,47 +24,77 @@ class WellnessWheel extends React.Component {
     }
     return circles.map((radius, index) => {
       return (
-        <circle cx="0" cy="0" r={radius} key={index} fill="#dee2e6" stroke="grey" />        
-      )
-    }
-    );
+        <circle
+          cx="0"
+          cy="0"
+          r={radius}
+          key={index}
+          fill="#dee2e6"
+          stroke="grey"
+        />
+      );
+    });
   }
 
   getCoordinatesForPercent(percent, radius) {
-      const x = radius * Math.cos(2 * Math.PI * percent);
-      const y = radius * Math.sin(2 * Math.PI * percent);
-      return [x, y];
-    }
+    const x = radius * Math.cos(2 * Math.PI * percent);
+    const y = radius * Math.sin(2 * Math.PI * percent);
+    return [x, y];
+  }
 
   drawLines() {
     let lines = [];
     const { bigRad, numberOfSlices } = this.state;
-    
+
     let cumulativePercent = 0;
-    for(let i = 0; i < numberOfSlices; i++) {
+    for (let i = 0; i < numberOfSlices; i++) {
       lines.push(cumulativePercent);
       cumulativePercent += 1 / numberOfSlices;
     }
 
     return lines.map((line) => {
       const [x1, y1] = this.getCoordinatesForPercent(line, bigRad);
-      return (
-        <line x1={x1} y1={y1} x2={0} y2={0} stroke="grey" key={line} />
-      )
-    })
+      return <line x1={x1} y1={y1} x2={0} y2={0} stroke="grey" key={line} />;
+    });
   }
 
   addLabels() {
-    const dimensions = ["financial", "emotional", "occupational", "physical", "intellectual", "social", "spiritual", "environmental"]
-    return (
-      <text x="20" y="35" transform="rotate(45)">text</text>
-    )
+    const labels = [
+      "financial",
+      "emotional",
+      "occupational",
+      "physical",
+      "intellectual",
+      "social",
+      "spiritual",
+      "environmental",
+    ];
+    const labelPosition = [];
+    const { numberOfSlices, bigRad } = this.state;
+    let cumulativePercent = 0.5 / numberOfSlices;
+
+    for (let i = 0; i < labels.length; i++) {
+      labelPosition.push(cumulativePercent);
+      cumulativePercent += 1 / numberOfSlices;
+    }
+
+    return labelPosition.map((position, index) => {
+      console.log(position);
+      let [textX, textY] = this.getCoordinatesForPercent(position, bigRad);
+      textX = textX > 0 ? textX + 0.2 * bigRad : textX - 0.7 * bigRad;
+
+      return (
+        <text x={textX} y={textY} key={labels[index]}>
+          {labels[index]}
+        </text>
+      );
+    });
   }
 
   slice() {
     let slices = [];
     const { radiiArr, numberOfSlices, colorArr } = this.state;
-    console.log(radiiArr);
+
     for (let i = 0; i < numberOfSlices; i++) {
       slices.push({
         percent: 1 / numberOfSlices,
@@ -66,12 +104,6 @@ class WellnessWheel extends React.Component {
     }
 
     let cumulativePercent = 0;
-
-    // function getCoordinatesForPercent(percent, radius) {
-    //   const x = radius * Math.cos(2 * Math.PI * percent);
-    //   const y = radius * Math.sin(2 * Math.PI * percent);
-    //   return [x, y];
-    // }
 
     return slices.map((slice) => {
       const [startX, startY] = this.getCoordinatesForPercent(
@@ -97,16 +129,17 @@ class WellnessWheel extends React.Component {
     });
   }
 
-
   render() {
     const { bigRad } = this.state;
     return (
       // <svg viewBox="-105 -105 210 210">
       <svg
         viewBox={`
-          ${-bigRad - 5} ${-bigRad - 5} ${bigRad * 2 + 10} ${bigRad * 2 + 10}
+          ${-bigRad - 100} ${-bigRad - 100} ${bigRad * 2 + 200} ${
+          bigRad * 2 + 200
+        }
         `}
-        width={200}
+        width={400}
       >
         {this.drawCircle(bigRad)}
         {this.slice()}
