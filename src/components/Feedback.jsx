7cac31065c608c710dcab2a524456ea4970819be
Iryna Shymbor <i.shymbor@gmail.com>
2020-11-06 18:13:03 +1100

    printing out feedback with improvement tips

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,19 +1,60 @@
 import React from "react";
+import { improve } from "./Texts.js";
 
 class Feedback extends React.Component {
   checkResults() {
     const results = Object.values(this.props.results);
     if (results.every((num) => num < 4)) {
-      return <p>You're struggling</p>;
+      return (
+        <p>
+          It seems that you're struggling, your answers indicate some potential
+          health and well-being risks. Here are some tips how you can improve
+          wellness areas:
+        </p>
+      );
     } else if (results.every((num) => num > 3)) {
-      return <p>You're thriving</p>;
+      return <p>Awesome, you're thriving! Well done!</p>;
     } else {
-      return <p>You're doing well in some fields</p>;
+      return (
+        <p>
+          Not bad, but there is room for improvement! Review the areas where you
+          are struggling. Here are some tips how you can improve them:
+        </p>
+      );
     }
+  }
+  renderList(phrase) {
+    const list = phrase.split(". ");
+    console.log(list);
+    console.log(phrase);
+    return list.map((sentence) => {
+      console.log(sentence);
+      return <li key={sentence}>{sentence}</li>;
+    });
+  }
+  renderImprove() {
+    const results = Object.entries(this.props.results)
+      .filter((pair) => pair[1] < 4)
+      .map((el) => el[0]);
+    const improvements = improve[0];
+    console.log(improvements);
+    return results.map((result) => {
+      return (
+        <div key={[`result`, result].join("")}>
+          <p>{result.toUpperCase()}:</p>
+          <ul>{this.renderList(improvements[result])}</ul>
+        </div>
+      );
+    });
   }
 
   render() {
-    return <>{this.checkResults()}</>;
+    return (
+      <div className="feedback">
+        {this.checkResults()}
+        {this.renderImprove()}
+      </div>
+    );
   }
 }
 
--- src/components/Texts.js
+++ src/components/Texts.js
@@ -22,13 +22,21 @@ export const descript = [
 
 export const improve = [
   {
-    financial: " financial ",
-    emotional: "feelings and emotions",
-    occupational: "job and career",
-    physical: "physical ",
-    intellectual: "mentally-stimulating ",
-    social: "community",
-    spiritual: "mindful and kind.",
-    environmental: "environment ",
+    financial:
+      "Create a budget that works for you. Consider rolling your debts into one. Try to save a bit of money regularly. Set aside some emergency cash. Be open to talking money with your partner",
+    emotional:
+      "Learn relaxation and stress management techniques. Cultivate an awareness of your thoughts and feelings through mind-body tools and programs. Develop confidence in your abilities. Practice expressing both negative and positive feelings constructively, with assertive communication. Take responsibility for and learn from your decisions, without self-critical thoughts",
+    occupational:
+      "Don't settle, keep motivated, and work towards what you want. Increase your knowledge and skills to accomplish your goals. Find the benefits and positives in your current job. Enjoy what you do, do what you enjoy. Create connections with your co-workers",
+    physical:
+      "Obtain prompt care for injuries and illnesses. Practice good self-care consistently, like eating healthy foods, exercising regularly and getting enough sleep. Practice behaviors that support your immune system and prevent illness (e.g., hand-washing, getting immunizations). Take care of your sexual and reproductive health (e.g., using barrier methods during sexual activity, getting regular exams). Pay attention to and support your emotional wellness. Limit your use of alcohol, if you choose to drink at all. Avoid tobacco products and illegal drugs; take medications only as prescribed",
+    intellectual:
+      "Listen to and consider the perspectives of others to improve critical thinking. Attend conferences, talks and programs that increase your knowledge in various areas. Seek opportunities to learn practical skills that you or a friend might need. Participate in new activities or groups to stay open-minded and expand your horizons. Pursue opportunities to experience a new language or culture",
+    social:
+      "Build trust and show support in relationships with friends, family members and significant others. Learn how to communicate effectively with others and understand your communication style. Reflect on personal conflicts and identify areas for improvement. Respect the needs and wants of others, and yourself. Make it a point to interact with people from diverse backgrounds, beliefs and cultures. Join a club, team or group that share your interests",
+    spiritual:
+      "Be curious about the world around you and the life experiences you encounter, through mindfulness techniques. Make time for personal reflection and defining your personal values (e.g., meditation, yoga). Actively care for the welfare of others and act with compassion. Behave in ways that are congruent with your values and beliefs. If you are religious, find a faith community or activity you are comfortable with and participate on a regular basis",
+    environmental:
+      "Create environments that support well-being at every opportunity (e.g., when you're hosting a party). Call out disrespectful or harassing behaviors and comments that create a negative social environment. Use the health, wellness and safety resources available to you. Practice environmentally conscious behaviors (e.g., recycling, energy conservation)",
   },
 ];
