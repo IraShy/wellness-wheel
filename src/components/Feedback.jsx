2f34b60d5407a31aa0865a458b2a56b49d238429
Iryna Shymbor <i.shymbor@gmail.com>
2020-11-10 15:13:51 +1100

    removed unused const in Feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { improve } from "../shared/Texts.js";
 import CheckResults from "./CheckResults";
 import RenderTips from "./RenderTips.jsx";
 
 class Feedback extends React.Component {
-
   render() {
     return (
       <div className="feedback">
-        <CheckResults results={Object.values(this.props.results)}/>
+        <CheckResults results={Object.values(this.props.results)} />
         <RenderTips results={Object.entries(this.props.results)} />
       </div>
     );
